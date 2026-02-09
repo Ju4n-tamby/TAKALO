6209e6bef7806dda98a7a4ce696c0058b4f6a75f
harryjonespascal023 <harryjojopascal@gmail.com>
2026-02-09 18:15:22 +0300

    Add login view and update routes for login functionality

--- app/config/routes.php
+++ app/config/routes.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-use Flight;
+
 use flight\Engine;
 use flight\net\Router;
 
@@ -10,3 +10,7 @@ use flight\net\Router;
  * @var Router $router
  * @var Engine $app
  */
+
+Flight::route('/', function () {
+    Flight::render('login');
+});
--- app/repository/UserRepository.php
+++ app/repository/UserRepository.php
@@ -4,5 +4,6 @@ namespace App\repository;
 
 class UserRepository
 {
+    
 
 }
